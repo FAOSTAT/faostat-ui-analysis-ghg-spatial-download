a560ac81a3d169c677798c28414368f19e4bb2b9
simonefenix <simone.murzilli@fao.org>
2015-05-04 11:32:51 +0200

    added multilanguage

---
+++ nls/F/translate.js
@@ -0,0 +1,33 @@
+define({
+    root : {
+        error: "Error",
+        ghg_spatial_download: "GHG Spatial Download",
+        download_layers: "Download layers",
+        select_a_country: "Select a country",
+        product_list: "Product's list",
+        product_not_available_for_country: "The product is not available for the selected country",
+
+        // data types
+        processed_activity_data: "Processed Activity Data",
+        processed_activity_data_description: "processed_activity_data_description",
+        parameters_emission_factors: "Parameters and emission factors",
+        parameters_emission_factors_description: "Parameters and emission factors",
+
+        // products
+        gfed4_burned_area_yearly: "GFED4 burned areas (yearly)",
+        burned_dry_matter: "Burned dry matter",
+        organic_soils_area: "Organic Soils Area",
+        emission_factors: "Emission Factors",
+        fuel_biomass_factors: "Fuel biomass factors",
+
+        // layers
+        humid_tropical_forest: 'humid_tropical_forest',
+        other_forest: 'other_forest',
+
+        organic_soils: 'organic soils',
+        cropland_organic_soils: 'cropland organic soils',
+        grassland_organic_soils: 'grassland organic soils'
+    },
+    fr : true,
+    es : true
+});
