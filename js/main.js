d9d518602f10342e64d5da482211a7e671f53f8a
simonefenix <simone.murzilli@fao.org>
2015-05-11 18:43:24 +0200

    fixed ui

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ define([
         'handlebars',
         'text!faostat-ui-analysis-ghg-spatial-download/html/templates.html',
         'i18n!faostat-ui-analysis-ghg-spatial-download/nls/translate',
-        'text!faostat-ui-analysis-ghg-spatial-download/config/data.json',
+
+        // TODO Change it
+        //'text!faostat-ui-analysis-ghg-spatial-download/config/data.json',
+        'text!geobricks_ui_distribution/config/data.json',
         //'jstree',
         'chosen',
         'sweetAlert'
@@ -20,6 +23,7 @@ define([
         this.CONFIG = {
             placeholder: 'tiles_container',
             datasource: 'faostatdb',
+            lang: 'E',
             lang_faostat: 'E',
 
             url_rest: 'http://faostat3.fao.org/wds/rest/procedures/usp_GetListBox',
@@ -32,14 +36,18 @@ define([
             country_selector_id: 'country_selector_id',
             country_selector_dd_id: 'country_selector_dd_id',
             accordion_datatype_id: 'accordion_datatype_id',
-            accordion_products_id: 'accordion_products_id'
+            accordion_products_id: 'accordion_products_id',
+            
+            // countries
+            url_wds_table_json: "http://faostat3.fao.org/wds/rest/table/json",
+            query_countries: "SELECT DISTINCT A.AreaCode AS Code, A.AreaName{{lang}} AS Name FROM DomainAreaList AS DA, Area AS A  WHERE DA.DomainCode = 'QC' AND (A.AreaCode = DA.AreaCode ) AND A.AreaCode IN ('238', '87', '97', '33', '273', '219', '260', '64', '237', '72', '154', '38', '207', '54', '200', '3', '208', '147', '195', '36', '163', '167', '166', '264', '184', '63', '128', '106', '91', '133', '20', '103', '220', '75', '251', '192', '19', '201', '48', '2', '28', '211', '57', '173', '131', '59', '156', '58', '172', '213', '151', '239', '221', '222', '114', '299', '70', '270', '27', '11', '216', '21', '37', '188', '129', '16', '100', '18', '85', '7', '13', '50', '138', '214', '125', '145', '89', '223', '96', '244', '226', '182', '250', '8', '203', '235', '95', '86', '160', '174', '193', '190', '66', '53', '215', '256', '141', '110', '120', '137', '189', '61', '198', '149', '169', '32', '140', '118', '180', '4', '225', '74', '218', '47', '178', '84', '40', '35', '233', '176', '10', '236', '122', '134', '124', '55', '230', '102', '231', '39', '217', '199', '179', '115', '229', '162', '82', '202', '196', '144', '73', '104', '157', '93', '205', '123', '175', '9', '191', '177', '153', '112', '79', '148', '105', '49', '88', '143', '108', '90', '170', '161', '23', '258', '159', '68', '127', '1', '183', '130', '52', '6', '234', '44', '209', '210', '272', '276', '117', '65', '26', '187', '25', '142', '136', '101', '69', '116', '119', '224', '150', '14', '168', '29', '94', '240', '46', '194', '22', '227', '181', '135', '45', '81', '126', '146', '17', '107', '113', '121', '249', '41', '185', '212', '155', '12', '5', '67', '255', '109', '132', '277', '56', '60', '83', '165', '158', '243', '98', '99', '80', '171', '197') AND A.AreaLevel = 5 ORDER BY A.AreaName{{lang}} ASC"
         };
     }
 
     GHG_SPATIAL_DOWNLOAD.prototype.init = function(config) {
         this.CONFIG.data = $.parseJSON(data);
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
-        this.render()
+        this.render();
     };
 
     GHG_SPATIAL_DOWNLOAD.prototype.render = function() {
@@ -68,10 +76,24 @@ define([
     GHG_SPATIAL_DOWNLOAD.prototype.renderCountryDropdown = function(id) {
         /* REST URL */
         var _this = this;
-        this.tree = $("#" + id);
-        var url = this.CONFIG.url_rest + '/' + this.CONFIG.datasource + '/QC/1/1/' + this.CONFIG.lang_faostat;
+        var url = this.CONFIG.url_wds_table_json;
+        var template = Handlebars.compile(this.CONFIG.query_countries);
+        var dynamic_data = {
+            lang: this.CONFIG.lang_faostat
+        };
+        var data = {};
+        var sql = {};
+
+        sql.limit = null;
+        sql.query = template(dynamic_data);
+        sql.frequency = "NONE";
+        data.datasource =this.CONFIG.datasource;
+        data.thousandSeparator = ',';
+        data.decimalSeparator = '.';
+        data.json = JSON.stringify(sql);
         $.ajax({
-            type: 'GET',
+            type: 'POST',
+            data : data,
             url: url,
             success: function (response) {
                 /* Cast the response to JSON, if needed. */
@@ -195,6 +217,7 @@ define([
             download_layers: translate.download_layers,
             download_button_id: data.downloadID
         };
+
         var html = template(dynamic_data);
         $("#" + id).append(html);
 
@@ -235,8 +258,11 @@ define([
                 coding_system: coding_system,
                 code: code
             };
+            console.log(dynamic_data);
             var url = template(dynamic_data);
+            console.log(url);
             if (this.urlExists(url)) {
+                console.log(url);
                 window.open(url);
                 //this.downloadURI(url);
             }
