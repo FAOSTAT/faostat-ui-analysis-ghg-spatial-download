31a1627d76e5eb1836f0edf73e8ca6ad3a2d7558
simonefenix <simone.murzilli@fao.org>
2015-05-18 13:39:18 +0200

    added i18n

--- nls/E/translate.js
+++ nls/E/translate.js
@@ -1,3 +1,11 @@
 define({
 
+    ghg_spatial_download: "Descargas espaciales de GEI",
+    download_layers: "Descargar capas",
+    select_a_country: "Seleccionar un país",
+    product_list: "Lista de productos",
+
+    // data types
+    processed_activity_data: "Datos de actividad procesados",
+    parameters_emission_factors: "Parámetros y factores de emisión",
 });
